74f3e11125bf6c61a608b3e467301087603be286
Hosu <hsleecode@gmail.com>
2026-07-24 14:34:07 -0700

    Add Divi child theme with Google Fonts and brand color variables

---
+++ functions.php
@@ -0,0 +1,7 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'divi_child_enqueue_styles' );
+function divi_child_enqueue_styles() {
+	wp_enqueue_style( 'divi-google-fonts', 'https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap', array(), null );
+	wp_enqueue_style( 'divi-parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'divi-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'divi-parent-style' ), wp_get_theme()->get('Version') );
+}
