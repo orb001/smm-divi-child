011d9b7dfa7169d52c740cab6b8e5a2c27277cf9
Hosu <hsleecode@gmail.com>
2026-07-28 15:25:58 -0700

    Add floating glass pill navbar styling
    Styles the header menu module into a frosted-glass pill with the
    brand-colored CTA button, matching the design reference, and ignores
    the local designs/ folder.

--- functions.php
+++ functions.php
@@ -4,4 +4,5 @@ function divi_child_enqueue_styles() {
 	wp_enqueue_style( 'divi-google-fonts', 'https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap', array(), null );
 	wp_enqueue_style( 'divi-parent-style', get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'divi-child-style', get_stylesheet_directory_uri() . '/style.css', array( 'divi-parent-style' ), wp_get_theme()->get('Version') );
+	wp_enqueue_style( 'divi-child-nav', get_stylesheet_directory_uri() . '/nav.css', array( 'divi-child-style' ), wp_get_theme()->get('Version') );
 }
